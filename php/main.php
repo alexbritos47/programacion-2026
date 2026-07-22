bb1b1f2889dd609df559450fb7b79a0d4d0504ad
alexbritos47 <onfreealex@gmail.com>
2026-07-22 15:17:52 -0300

    php

---
+++ php/main.php
@@ -0,0 +1,82 @@
+<?php
+
+//ejercicio 1 con nahuel alba 
+
+//$nombre = "ana";
+//$edad = 17;
+//$cuidad = "libertad";
+
+
+
+//echo "me llamo $nombre" . " tengo $edad" . " y vivo en $cuidad" ;
+
+
+
+//ejercicio 2 con nahuel alba 
+ 
+
+//$nombre = "teclado";
+//$precio = 1200;
+//$stock = 5; 
+
+//echo "nombre del producto: $nombre" .  " su precio es $precio"  .  " queadan $stock unidades";
+
+
+//ejercicio 3 con nahuel alba 
+
+//$nombredeljugador = "mateo";
+//$nombredelpersonaje = "dragonX";
+//$nivel = 25;
+//$servidor = "latinoamerica";
+
+//echo "=== PERFIL DEL JUGADOR ===\n";
+//echo "$nombredeljugador\n";
+//echo "personaje:$nombredelpersonaje\n";
+//echo "💪:$nivel\n";
+//echo "🌍: $servidor\n";
+//echo "========================\n";
+
+
+//ejercicio 4 con nahuel alba 
+
+//$nombre = "Lucía";
+//$curso = "Tercero";
+//$materia = "Programación";
+
+//echo "Nombre:$nombre\n" ;
+//echo "Curso:$curso \n";
+//echo "Materia:$materia \n";
+
+
+//BLOQUE 2 parte 1 con Nahuel alba
+
+
+//$numero1 = 1;
+//$numero2 = 33;
+//$total=$numero1 + $numero2 ;// 34
+ //echo $calculo=($numero1+$numero2);
+
+ //$total=$numero1 - $numero2 ;// 32
+ //echo $calculo=($numero1-$numero2);
+
+ //$total=$numero1 * $numero2 ;// 33
+ //echo $calculo=($numero1*$numero2);
+
+ //$total=$numero1 / $numero2 ;// 22
+ //echo $calculo=($numero1/$numero2);
+
+//ejercicio 2 con nahuel 
+
+//$base = 2;
+//$altura= 2;
+
+//echo $area = "su area es:" ($base * $altura);
+
+$nota1 = 5;
+$nota2 = 10;
+$nota3 = 3; 
+echo "su nota es :" ($nota1 + $nota2 + $nota3) / 3;
+
+
+
+
